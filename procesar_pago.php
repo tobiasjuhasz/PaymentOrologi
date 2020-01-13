e9ab1eab2b96b0baabe1d4ce2f3f125ca03b001e
Tobias <tobiasjuhasz@gmail.com>
2020-01-13 02:14:06 -0300

    prueba

--- procesar_pago.php
+++ procesar_pago.php
@@ -20,19 +20,19 @@
     $amount = $_POST['amount'];
     
     MercadoPago\SDK::setAccessToken("TEST-6540974822759376-011204-1f1514c4ee488176b1fed65bd4e0e79b-238754877");
-    //...
-    $payment = new MercadoPago\Payment();
-    $payment->transaction_amount = 110;
-    $payment->token = $token;
-    $payment->description = "OrologiFB - " . $desc;
-    $payment->installments = $installments;
-    $payment->payment_method_id = $payment_method_id;
-    $payment->payer = array(
-        "email" => $email
-    );
-    // Save and posting the payment
-    $payment->save();
-    //...
-    // Print the payment status
-    echo $payment->status;
+    // //...
+    // $payment = new MercadoPago\Payment();
+    // $payment->transaction_amount = 110;
+    // $payment->token = $token;
+    // $payment->description = "OrologiFB - " . $desc;
+    // $payment->installments = $installments;
+    // $payment->payment_method_id = $payment_method_id;
+    // $payment->payer = array(
+    //     "email" => $email
+    // );
+    // // Save and posting the payment
+    // $payment->save();
+    // //...
+    // // Print the payment status
+    // echo $payment->status;
 ?>
